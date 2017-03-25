57fa1b5b242ceb5ab283a007c019418b31b074d4
mitakeck <mitake.ck@gmail.com>
2017-03-25 22:28:44 +0900

    update test code

--- test.rb
+++ test.rb
@@ -1,15 +1,46 @@
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 require './nodeparser.rb'
 
-url = 'https://news.ycombinator.com/'
-charset = nil
-html = open(url) do |f|
-  charset = f.charset
-  f.read
+file = File.read('sample/hacker_news.json')
+struct = JSON.parser.new(file).parse
+# url = 'https://news.ycombinator.com/'
+# charset = nil
+# html = open(url) do |f|
+#   charset = f.charset
+#   f.read
+# end
+# doc = Nokogiri::HTML.parse(html, nil, charset)
+
+def serializer(struct)
+  out = Hash.new
+  struct.each do |key, value|
+    p '-' * 30
+    tkey = ''
+    if match = key.match(/(?<k1>([^:]*)):`(?<k2>([^`]*))`/)
+      if is_array = match[:k1].match(/\[\].*/)
+        p 'array key'
+      end
+      tkey = match[:k1]
+      p match[:k2]
+    else
+      raise InvalidSyntaxKey
+    end
+
+    case value.to_s
+    when 'string'
+      p 'string'
+    when /^{.*}$/
+      p 'object'
+      serializer(struct[key])
+    else
+      raise InvalidSyntaxValue
+    end
+  end
+
+  out
 end
 
-doc = Nokogiri::HTML.parse(html, nil, charset)
-np = NodeParser.new('#\31 3936153 > td:nth-child(3) > a text{}')
-p np.parse(doc)
+serializer(struct)
