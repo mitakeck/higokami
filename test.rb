40f8a08450ecc046cfd5073f2aeb97951370fbd8
mitakeck <mitake.ck@gmail.com>
2017-04-02 23:47:37 +0900

    update sample code

--- test.rb
+++ test.rb
@@ -3,21 +3,3 @@ require './higokami.rb'
 # parse hacker news
 higokami = Higokami.new('sample/news.ycombinator.com/index.json')
 puts higokami.parse('https://news.ycombinator.com/')
-
-`
-$ ruby test.rb
-=> {
-  "title": "Hacker News",
-  "article": [
-    {
-      "title": "Deep Photo Style Transfergithub.com",
-      "link": "https://github.com/luanfuj..."
-    },
-    {
-      "title": "Gcam, the computational photogra...",
-      "link": "https://blog.x.company/mee..."
-    },
-    ...
-  ]
-}
-`
